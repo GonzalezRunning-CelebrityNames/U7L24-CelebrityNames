a48d7e8a09442fc7bccb984fa8ed1dc601c280a1
runningm <rennenaway@gmail.com>
2015-01-22 10:11:26 -0800

    Should work
    Please work

--- CelebrityNamesBirth.java
+++ CelebrityNamesBirth.java
@@ -7,7 +7,7 @@ import java.util.*;
  * @author Roger Jaffe
  * @version 2015-01-19
  */
-public class CelebrityNames
+public class CelebrityNamesBirth
 {
     public static final String FILE_NAME = "CelebrityData.txt";
     public static void main(String args[]) throws IOException
@@ -34,7 +34,7 @@ public class CelebrityNames
             String lastName = sc.next( );
             String birthDate = sc.next();
             
-            reversedName[j] = lastName + ", " + firstName + " " + middleName + " -- " + birthDate;
+            reversedName[j] = birthDate  + " -- " + lastName + ", " + firstName + " " + middleName ;
         } 
         
         Arrays.sort(reversedName);
